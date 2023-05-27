729d95ae78cfe1d76832c37b2346417a0f61fa62
Niginaxon <134757342+Niginaxon@users.noreply.github.com>
2023-05-27 08:58:10 +0500

    Kiritsh-chiqarish
    1-masala

---
+++ Begin/Begin/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, S;
+            S = a * a;
+            Console.WriteLine(P);
+            Console.ReadKey();
+
+        }
+    }
+}
