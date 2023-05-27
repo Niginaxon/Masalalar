a87fe168e735ce8d87436ad7269ade792107656e
Niginaxon <134757342+Niginaxon@users.noreply.github.com>
2023-05-27 09:00:01 +0500

    Kiritish-chiqarish
    2-masala

---
+++ Begin2/Begin2/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Begin2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int a, s;
+            a = Convert.ToInt32(Console.ReadLine());
+            s = a * a;
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
